b4f194e3ac2f337c5851b607d309b92e6041baba
Druva D <ddruva445@gmail.com>
2023-05-17 20:30:29 +0530

    Added autograder script and endpoints

--- src/index.py
+++ src/index.py
@@ -1,13 +1,21 @@
-from fastapi import FastAPI
+import os
+from typing import List
+from fastapi import FastAPI, Request
+from fastapi import UploadFile
+import aiofiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-from src.dtos.ISayHelloDto import ISayHelloDto
+from src.tester.autograder import autoeval
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/hello/{name}")
@@ -15,6 +23,55 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-@app.post("/hello")
-async def hello_message(dto: ISayHelloDto):
-    return {"message": f"Hello {dto.message}"}
+@app.post("/upload_files")
+async def upload_program(files: List[UploadFile]):
+    c = False
+    json = False
+    if len(files) != 2:
+        return {
+            "Result": "ERROR",
+            "filenames": [file.filename for file in files],
+            "error": "Wrong number of files",
+        }
+    if not os.path.exists("./files"):
+        os.mkdir("./files")
+    for file in files:
+        if file.filename.endswith(".c"):
+            destination_file_path = "./files/test.c"
+            async with aiofiles.open(destination_file_path, "wb") as out_file:
+                while content := await file.read(1024):  # async read file chunk
+                    await out_file.write(content)  # async write file chunk
+            c = True
+        elif file.filename.endswith(".json"):
+            destination_file_path = "./files/test.json"
+            async with aiofiles.open(destination_file_path, "wb") as out_file:
+                while content := await file.read(1024):
+                    await out_file.write(content)
+            json = True
+
+    if c and json:
+        return {"Result": "OK", "filenames": [file.filename for file in files]}
+    else:
+        if not c:
+            return {
+                "Result": "ERROR",
+                "filenames": [file.filename for file in files],
+                "error": "No C file",
+            }
+        else:
+            return {
+                "Result": "ERROR",
+                "filenames": [file.filename for file in files],
+                "error": "No JSON file",
+            }
+
+
+@app.get("/test")
+async def test():
+    if os.path.exists("./files/test.c") and os.path.exists("./files/test.json"):
+        result = autoeval("./files/test.c", "./files/test.json")
+        os.remove("./files/test.c")
+        os.remove("./files/test.json")
+        return {"Result": "OK", "content": result[0], "score": result[1]}
+    else:
+        return {"Result": "ERROR", "content": "Files don't exist"}
---
+++ src/tester/autograder.py
@@ -0,0 +1,113 @@
+import csv
+import json
+import os
+import subprocess
+import sys
+
+# To call this script from the command line, run this:
+# python3 autograder.py <path_of_c_code_file> <path_of_testcase_json_file> -flag
+# use the -i flag to operate in instructor mode
+# use the -t flag to operate in testcase generator mode
+
+# To call this script in the batch processing mode, use:
+# python3 autograder.py <path_of_testcase_json_file> -b
+
+
+def autoeval(code_path, testcases_path, flag=None) -> tuple:
+    score = 0
+    testcase_num = 0
+    output = ""
+
+    # Batch processing mode
+    # currently appends new entry to end of csv, and doesn't make a new csv if it already exists
+    # Loads testcases from the json file as a python dict
+
+    with open(testcases_path, "r") as f:
+        testcases = json.load(f)
+
+    # Prints code file name
+    output += (
+        "Testing code for student with BITS ID: "
+        + os.path.splitext(os.path.basename(code_path))[0]
+        + "\n"
+    )
+
+    # Compiles C code
+    # Compilation errors need to be handled here
+    compilation_process = subprocess.run(
+        "gcc " + code_path,
+        text=True,
+        timeout=5,
+        shell=True,
+        capture_output=True,
+    )
+
+    if not os.path.exists(
+        os.path.join(os.getcwd(), "a.out")
+    ):  # If compilation error occurs
+        output += "Compilation Error:\n"
+        print(compilation_process.stderr)
+        return output, 0  # Exit if compilation error in student mode
+
+    for test in testcases["test_cases"]:
+        testcase_num += 1
+        # Returns CompletedProcess instance with attributes args, returncode, stdout and stderr, all of type string.
+        process_stdin = test["input"]
+
+        try:
+            # Student mode
+            output += "-----TEST CASE NUMBER " + str(test["id"]) + "-----\n"
+            output += "---Given Input---\n"
+            output += test["input"] + "\n"
+            output += "---Expected Output---"
+            output += test["output"] + "\n"
+            output += "---Your Output---"
+
+            # Terminate process after 5 seconds
+            completed_process = subprocess.run(
+                "./a.out",
+                capture_output=True,
+                input=process_stdin,
+                text=True,
+                timeout=test["time_out"],
+                shell=True,
+                check=False,
+            )
+
+            process_stdout = completed_process.stdout
+
+        except subprocess.TimeoutExpired:
+            # Student mode or Test Case Generator mode
+            output += "Timeout on test case " + str(test["id"]) + "\n"
+            output += "---Result: Failed---" + "\n"
+
+        except subprocess.CalledProcessError as error:
+            # Student mode and Test Case Generator mode
+            output += ("Exception: ", error.stderr + "\n")
+            output += "---Result: Failed---" + "\n"
+
+        except Exception as error:
+            # All other exceptions
+            # Unlikely that any exceptions other than timeout or runtime errors occur, but this handles it
+            if flag == None or flag == "-t":
+                # Student mode and Test Case Generator mode
+                output += "Exception:" + str(error) + "\n"
+                output += "---Result: Failed---" + "\n"
+
+        else:
+
+            testcase_passed = process_stdout == test["output"]
+            output += process_stdout + "\n"
+            result = "Passed" if process_stdout == test["output"] else "Failed" 
+            output += "---Result: " + result + "---" + "\n"
+
+            # Increases score if stdout and expected output match
+            score += int(testcase_passed)
+
+    output += "\nFinal Score:" + str(score) + "/" + str(testcase_num)
+
+    # Deletes a.out after evaluation
+    if os.path.exists("a.out"):
+        os.remove("a.out")
+
+    return output, score
